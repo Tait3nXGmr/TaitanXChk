50130ec742afe144bd6cdded2332ddbdbdab2f5a
Tait3nXGmr <164100772+Tait3nXGmr@users.noreply.github.com>
2024-03-22 08:59:03 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+import re
+from os import getenv
+
+from dotenv import load_dotenv
+from pyrogram import filters
+
+load_dotenv()
+
+
+API_ID = int(getenv("API_ID", ""))
+
+API_HASH = getenv("API_HASH", "")
+
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+
+MONGO_DB_URI = getenv("MONGO_DB_URI", "")
+
+BOT_USERNAME = getenv("BOT_USERNAME", "")
+
+OWNER_ID = getenv("OWNER_ID", "")
